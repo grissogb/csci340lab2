fff10f3113ba686590f8b84feb02cafa1453f77d
Garrett <grissogb@hendrix.edu>
2021-09-13 09:18:42 -0500

    Fixed Cat, added numbers, need color and layout
    Should've done a commit a while ago

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ $(document).ready(function(){
         if (results["url"].endsWith(".mp4")) {
           $("#dog").attr("src", "images/blank.jpg");
         } else {
+          $("#dogcount").html(0);
           $("#dog").attr("src", results["url"]);
+          var catcounthtml = Number($("#catcount").html());
+          $("#catcount").html(catcounthtml+1);
         }
       },
       error: function(xhr,status,error) {
@@ -22,13 +25,16 @@ $(document).ready(function(){
   $("#catclicker").click(function(){
     $.ajax({
       datatype: "json",
-      url: "http://random.cat/meow",
+      url: "https://thatcopy.pw/catapi/rest/",
       success: function(results) {
         console.log(results["url"]);
         if (results["url"].endsWith(".mp4")) {
           $("#cat").attr("src", "images/blank.jpg");
         } else {
+          $("#catcount").html("0");
           $("#cat").attr("src", results["url"]);
+          var dogcounthtml = Number($("#dogcount").html());
+          $("#dogcount").html(dogcounthtml+1);
         }
       },
       error: function(xhr,status,error) {
